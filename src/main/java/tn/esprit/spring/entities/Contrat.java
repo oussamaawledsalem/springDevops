c4219c68e97725e24d03a4c236af059e179205cb
oussamaawledsalem <99668717+oussamaawledsalem@users.noreply.github.com>
2025-02-21 16:21:25 +0100

    Update Contrat.java

--- src/main/java/tn/esprit/spring/entities/Contrat.java
+++ src/main/java/tn/esprit/spring/entities/Contrat.java
@@ -15,76 +15,70 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Contrat implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long reference;
 
+    @Temporal(TemporalType.DATE)
+    private Date dateDebut;
+    
+    private String typeContrat;
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long reference;
-	
-	@Temporal(TemporalType.DATE)
-	private Date dateDebut;
-	 
-	private String typeContrat;
- 
-	@JsonIgnore
-	@OneToOne(mappedBy="contrat")
-		private static final long serialVersionUID = 1L;
+    @JsonIgnore
+    @OneToOne(mappedBy="contrat")
+    private Employe employe;
 
-	private Employe employe;
+    private float salaire;
 
-	private float salaire;
+    public Contrat() {
+        super();
+    }
 
-	public Contrat() {
-		super();
-	}
-	
-	public Contrat(Date dateDebut, String typeContrat, float salaire) {
-		this.dateDebut = dateDebut;
-		this.typeContrat = typeContrat;
-		this.salaire = salaire;
-	}
+    public Contrat(Date dateDebut, String typeContrat, float salaire) {
+        this.dateDebut = dateDebut;
+        this.typeContrat = typeContrat;
+        this.salaire = salaire;
+    }
 
+    public Date getDateDebut() {
+        return dateDebut;
+    }
 
-	public Date getDateDebut() {
-		return dateDebut;
-	}
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
 
-	public void setDateDebut(Date dateDebut) {
-		this.dateDebut = dateDebut;
-	}
- 
-	public Long getReference() {
-		return reference;
-	}
+    public Long getReference() {
+        return reference;
+    }
 
-	public void setReference(Long reference) {
-		this.reference = reference;
-	}
+    public void setReference(Long reference) {
+        this.reference = reference;
+    }
 
-	public String getTypeContrat() {
-		return typeContrat;
-	}
+    public String getTypeContrat() {
+        return typeContrat;
+    }
 
-	public void setTypeContrat(String typeContrat) {
-		this.typeContrat = typeContrat;
-	}
+    public void setTypeContrat(String typeContrat) {
+        this.typeContrat = typeContrat;
+    }
 
-	public float getSalaire() {
-		return salaire;
-	}
+    public float getSalaire() {
+        return salaire;
+    }
 
-	public void setSalaire(float salaire) {
-		this.salaire = salaire;
-	}
+    public void setSalaire(float salaire) {
+        this.salaire = salaire;
+    }
 
-	public Employe getEmploye() {
-		return employe;
-	}
+    public Employe getEmploye() {
+        return employe;
+    }
 
-	public void setEmploye(Employe employe) {
-		this.employe = employe;
-	}
- 
+    public void setEmploye(Employe employe) {
+        this.employe = employe;
+    }
 }
